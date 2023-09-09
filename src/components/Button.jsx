097427ab62a069c82c29c954be13055d0fbb9497
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-09 20:32:51 +0200

    made 2 components and partial logic

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ACTIONS } from '../App'
+
+function Button({ dispatch, digit }) {
+    return (
+        <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>{digit}</button>
+    )
+} 
+
+export default Button
